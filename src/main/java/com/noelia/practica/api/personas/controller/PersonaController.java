2202bb2c420d29860cc01e89512676e6cb6b480b
Noelia Neira <noeliasneira@gmail.com>
2020-05-08 22:00:10 -0300

    Se agrega método para agregar personas (@PostMapping)

--- src/main/java/com/noelia/practica/api/personas/controller/PersonaController.java
+++ src/main/java/com/noelia/practica/api/personas/controller/PersonaController.java
@@ -2,9 +2,7 @@ package com.noelia.practica.api.personas.controller;
 
 import com.noelia.practica.api.personas.model.GeneroEnum;
 import com.noelia.practica.api.personas.model.Persona;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -12,20 +10,41 @@ import java.util.List;
 @RestController
 @RequestMapping("/api/personas")
 public class PersonaController {
+    private ArrayList<Persona> personas;
 
-    @GetMapping
-    public List<Persona> getPersonas(){
-        Persona p1 = new Persona("Carla", "Gonzalez", 37, "argentina", GeneroEnum.FEMENINO);
-        Persona p2 = new Persona("Alberto", "Fernandez", 60, "argentina", GeneroEnum.MASCULINO);
-        Persona p3 = new Persona("Sol", "Perez", 23, "argentina", GeneroEnum.OTRO);
+    public PersonaController(){
+        personas = new ArrayList<Persona>();
 
-        ArrayList<Persona> personas = new ArrayList<>();
+        Persona p1 = new Persona(1,"Carla", "Gonzalez", 37, "argentina", GeneroEnum.FEMENINO);
+        Persona p2 = new Persona(2,"Alberto", "Fernandez", 60, "argentina", GeneroEnum.MASCULINO);
+        Persona p3 = new Persona(3,"Sol", "Perez", 23, "argentina", GeneroEnum.OTRO);
 
         personas.add(p1);
         personas.add(p2);
         personas.add(p3);
 
-        return personas;
+    }
 
+
+    @GetMapping
+    public List<Persona> getPersonas(){
+        return personas;
+    }
+
+    @GetMapping("/{id}")
+    public Persona getPesona(@PathVariable int id){
+        for (int i = 0; i < personas.size(); i++) {
+            Persona persona = personas.get(i);
+            if (id == persona.getId()){
+                return persona;
+            }
+        }
+        return null;
+    }
+
+    @PostMapping
+    public Persona addPersona(@RequestBody Persona persona){
+        personas.add(persona);
+        return persona;
     }
 }
--- src/main/java/com/noelia/practica/api/personas/model/Persona.java
+++ src/main/java/com/noelia/practica/api/personas/model/Persona.java
@@ -6,6 +6,7 @@ import lombok.Data;
 @AllArgsConstructor
 @Data
 public class Persona {
+    private int id;
     private String nombre;
     private String apellido;
     private int edad;
